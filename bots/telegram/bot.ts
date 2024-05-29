77e0e05b3adeefe164587c6447aa60fd9e37041a
sticky <sticky@fp>
2024-05-29 19:13:35 -0400

    feat: handle relay messages in telegram bot

--- bots/telegram/bot.ts
+++ bots/telegram/bot.ts
@@ -1,26 +1,115 @@
-import { Bot, Context, session } from "https://deno.land/x/grammy@v1.23.0/mod.ts";
-import { run, sequentialize } from "https://deno.land/x/grammy_runner@v2.0.3/mod.ts";
+import { Bot } from "https://deno.land/x/grammy@v1.23.0/mod.ts";
 
-import { sendToAgent } from "./helpers";
+// this is our frontend
 
-const bot = new Bot(process.env.TELEGRAM_BOT_KEY);
+const TELEGRAM_BOT_KEY = Deno.env.get("MY_VARIABLE");
 
-// Build a unique identifier for the `Context` object.
-function getSessionKey(ctx: Context) {
-  return ctx.chat?.id.toString();
+type User = {
+  id: string;
+  relayUrl: string;
 }
 
-// Sequentialize before accessing session data!
-bot.use(sequentialize(getSessionKey));
-bot.use(session({ getSessionKey }));
+type Users = {
+  [id: string]: User;
+};
 
-bot.command("start", (ctx: any) => ctx.reply("Howdy 👋! Let's get your Solidity contracts written so you can get back to sleep. How should the contracts work?"));
+const users: Users = {};
+const relays: { [wsUrl: string]: WebSocket } = {};
 
-// Reply to any message with "Hi there!".
+const bot = new Bot(TELEGRAM_BOT_KEY || "");
+
+// on start, create a container for the relay, connect to it and add handlers, store this and the user id
+bot.command("start", async (ctx: any) => {
+  if (isNewUser(ctx.from.id)) {
+    ctx.reply("Howdy 👋! Let's get your Solidity contracts written so you can get back to sleep. We're spinning up your AI devs to work on it now...");
+    await addNewUser(ctx.from.id);
+  } else {
+    ctx.reply("Let's start over!");
+    await restartAgents();
+  }
+});
+
+// on message from bot, forward it to the relay
 bot.on("message", (ctx: any) => {
-    console.log(ctx);
-    sendToAgent(ctx.message);
-    ctx.reply("Sending to the llm...");
+  sendToRelay(ctx, ctx.from.id, ctx.message);
 });
 
 bot.start();
+
+async function addNewUser(userId: string) {
+  // create container
+  const containerUrl = await startNewContainer();
+  // connect to it
+  const ws = connectToContainer(containerUrl);
+  // save the ws
+  relays[ws.url] = ws;
+  addHandlers(ws);
+  users[userId] = {
+    id: userId,
+    relayUrl: ws.url,
+  };
+}
+
+function connectToContainer(containerUrl: string): WebSocket {
+  const ws = new WebSocket(containerUrl);
+  return ws;
+}
+
+function sendToRelay(ctx: any, userId: string, message: string) {
+  const user = users[userId];
+  if (!user) {
+    console.log(`User ${userId} not found`);
+    ctx.reply("Hey stranger! Use the /start command to get started.");
+    return;
+  } else {
+    const ws = relays[user.relayUrl];
+    if (!ws) {
+      console.log(`Relay for user ${userId} not found`);
+      ctx.reply("Hey stranger! Use the /start command to get started.");
+      return;
+    }
+    ws.send(message);
+  }
+}
+
+// websocket handlers
+
+function addHandlers(ws: WebSocket) {
+  ws.onopen = handleConnect;
+  ws.onmessage = (event) => handleMessage(event, ws.url);
+}
+function handleConnect() {
+  console.log("Connected to a relay");
+}
+// on message from relay, send it to the bot
+async function handleMessage(event: MessageEvent, relayUrl: string) {
+  const data = event.data;
+  console.log(`Received from ${relayUrl}: message: ${data}`);
+
+  const user = getUserByRelayUrl(relayUrl);
+  if (!user) {
+    console.log(`Received message from unknown relay: ${relayUrl}`);
+    return;
+  } else {
+    await bot.api.sendMessage(user.id, data);
+  }
+};
+
+function isNewUser(userId: string) {
+  return !users[userId];
+}
+
+function startNewContainer(): string {
+  // TODO: start new container
+  return "https://localhost:8000";
+}
+
+function restartAgents(): string {
+  // TODO: start new container
+  return "https://localhost:8000";
+}
+
+function getUserByRelayUrl(relayUrl: string): User | undefined {
+  return Object.values(users).find((user) => user.relayUrl === relayUrl);
+}
+
