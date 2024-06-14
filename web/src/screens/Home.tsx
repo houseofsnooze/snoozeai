862aff312bf8a68b804da94b25e2c9397f68a337
sticky <sticky@fp>
2024-06-14 15:01:31 -0400

    feat(web): add youtube video to home screen

--- web/src/screens/Home.tsx
+++ web/src/screens/Home.tsx
@@ -1,24 +1,40 @@
 import { useState } from "react";
+import { YouTubeEmbed } from "@next/third-parties/google";
 import HomeButtons from "../components/HomeButtons";
 
 interface HomeProps {
-    setupSession: (addresses?: { relayAddress: string, agentAddress: string, snoozeApiKey: string }) => void;
+  setupSession: (addresses?: {
+    relayAddress: string;
+    agentAddress: string;
+    snoozeApiKey: string;
+  }) => void;
 }
 
 export default function Home({ setupSession }: HomeProps) {
   const [showHeader, setShowHeader] = useState(true);
 
   return (
-    <div className="pb-6 mb-4 relative w-[1000px] gap-4 flex place-items-center ">
+    <div className="container pb-6 mb-4 relative gap-4 flex place-items-center ">
       <div className="grid gap-4 flex">
         {showHeader && (
-          <h2 className="scroll-m-20 pb-2 text-3xl tracking-tight first:mt-0 text-foreground">
-            {"You want to launch on mainnet but you don't want to mess it up."}{" "}
-            <br /> <b className="text-yellow">Snooze</b>{" "}
-            {
-              "turns your wildest ideas into smart contracts then tests them rigorously so you are ready for deployment."
-            }
-          </h2>
+          <div className="grid grid-cols-2 gap-4">
+            <h2 className="col-span-1 scroll-m-20 pb-2 text-3xl tracking-tight text-foreground">
+              {
+                "You want to launch on mainnet but you don't want to mess it up."
+              }{" "}
+              <b className="text-yellow">Snooze</b>{" "}
+              {
+                "turns your wildest ideas into smart contracts then tests them rigorously so you are ready for deployment."
+              }
+            </h2>
+            <div className="col-span-1">
+              <YouTubeEmbed
+                videoid="vnH51OPYHiA"
+                height={200}
+                params="controls=0"
+              />
+            </div>
+          </div>
         )}
         <HomeButtons onSubmit={setupSession} setShowHeader={setShowHeader} />
       </div>
