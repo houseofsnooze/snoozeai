c86ff5363d9c0dae4ad46e082e8e175e4e55b076
exponential <exp7l@proton.me>
2024-07-02 10:58:41 +0100

    agent tidy

--- agent/zzz-template/hardhat.config.js
+++
@@ -1,8 +0,0 @@
-// Set up ethers for use in test
-// https://www.npmjs.com/package/@nomicfoundation/hardhat-ethers
-require("@nomicfoundation/hardhat-ethers");
-require("@nomicfoundation/hardhat-chai-matchers")
-
-module.exports = {
-  solidity: "0.8.26",
-};
--- bots/telegram/commands/index.ts
+++
@@ -1,1 +0,0 @@
-export { issueAccessCode } from "./issueAccessCode.ts";
